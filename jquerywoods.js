36d34c56eba7fb404c8769dd05e15146a3ba9f25
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-02 16:01:13 -0700

    Update and rename jquery.js to jquerywoods.js
    Added enchanted wood buttons.

--- jquerywoods.js
+++ jquerywoods.js
@@ -43,3 +43,25 @@ $(document).ready(function(){
 
 </body>
 </html>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").hide("slow", function(){
+      alert("Oh, goodness. Your magic made the text go away. Well done, young one! ");
+    });
+  });
+});
+</script>
+</head>
+<body>
+
+<button>Follow Me!</button>
+
+<p> You find yourself spinning in a mirrored wood. The trees are mirrored, the path is mirrored, and there are so many places to not go. Which way is home?</p>
+
+</body>
+</html>
