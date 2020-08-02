09ae3cd6d298b908df36275c6634f697980657dd
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-02 15:27:32 -0700

    Create jquery.js
    I'm creating options.

---
+++ jquery.js
@@ -0,0 +1,23 @@
+<head>
+<script src="jquery-3.5.1.min.js"></script>
+</head>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+</script>
+</head>
+<body>
+
+<p>This Way!</p>
+<p>That Way!</p>
+<p>Click your heels home!</p>
+
+</body>
+</html>
