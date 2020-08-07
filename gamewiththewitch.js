a8f4585a7a86da442c532e4096349c23dfb70b53
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-06 20:27:26 -0700

    Update gamewiththewitch.js

--- gamewiththewitch.js
+++ gamewiththewitch.js
@@ -1,3 +1,64 @@
+
+
+class Tooltip extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      displayTooltip: false
+    }
+    this.hideTooltip = this.hideTooltip.bind(this)
+    this.showTooltip = this.showTooltip.bind(this)
+  }
+  
+  hideTooltip () {
+    this.setState({displayTooltip: false})
+    
+  }
+  showTooltip () {
+    this.setState({displayTooltip: true})
+  }
+
+  render() {
+    let message = this.props.message
+    let position = this.props.position
+    return (
+      <span className='tooltip'
+          onMouseLeave={this.hideTooltip}
+        >
+        {this.state.displayTooltip &&
+        <div className={`tooltip-bubble tooltip-${position}`}>
+          <div className='tooltip-message'>{message}</div>
+        </div>
+        }
+        <span 
+          className='tooltip-trigger'
+          onMouseOver={this.showTooltip}
+          >
+          {this.props.children}
+        </span>
+      </span>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <p>Here is a <Tooltip message={'It is time to give up, buttercup!'} position={'top'}>tooltip</Tooltip> on top.</p>
+        <p>Here is a <Tooltip message={'Are you scared of your destiny?'} position={'bottom'}>tooltip</Tooltip> on bottom.</p>
+        <p>Here is a <Tooltip message={'You think you're going to win?'} position={'left'}>tooltip</Tooltip> on left.</p>
+        <p>Here is a <Tooltip message={'Did you want to play, pretty?'} position={'right'}>tooltip</Tooltip> on right.</p>
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
